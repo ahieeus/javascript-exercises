f1a254f9f77c18011204d0bfa3f9bcaac690972d
ahieeus <doxuanhieu28@gmail.com>
2024-11-27 16:09:58 +0700

    Edit

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (typeof(num1) != typeof(num2)) return "ERROR";
+    else {
+        if ((num1 * num2 < 0) || (Math.floor(num1) * Math.floor(num2) != num1 * num2)) return "ERROR";
+        else return (Math.abs(num1 - num2) +1) * (num1 + num2) / 2;
+    }
 };
 
 // Do not edit below this line
