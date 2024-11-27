f88676d837be4a15fb9016dd5a5d11097e2b263c
ahieeus <doxuanhieu28@gmail.com>
2024-11-27 15:32:19 +0700

    Edit

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reverStr = "";
+    for (let i = str.length; i >= 0; i--) {
+        reverStr += str.charAt(i);
+    }
+    return reverStr;
 };
+
+/*const reverseString = function(str) {
+    return str.split("").reverse().join("");
+}*/
 
 // Do not edit below this line
 module.exports = reverseString;
